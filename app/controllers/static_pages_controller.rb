6d4f9ab797cc4b0330b87a87c6b89f0438a641de
Nick Gerleman <nick@nickgerleman.com>
2013-04-27 23:03:16 -0500

    testing caching options

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,19 +1,19 @@
 class StaticPagesController < ApplicationController
   def home
-    expires_in 5.minutes, public: true
+    expires_in 1.hour, public: true
   end
 
   def about
-    expires_in 5.minutes, public: true
+    expires_in 1.hour, public: true
 
   end
 
   def features
-    expires_in 5.minutes, public: true
+    expires_in 1.hour, public: true
 
   end
 
   def contribute
-    expires_in 5.minutes, public: true
+    expires_in 1.hour, public: true
   end
 end
