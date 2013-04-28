58b6602bd85c2d1fb155a7443c3bf82ac3a4ca51
Nick Gerleman <nick@nickgerleman.com>
2013-04-27 22:57:29 -0500

    testing caching options

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,17 +1,19 @@
 class StaticPagesController < ApplicationController
   def home
-
+    expires_in 5.minutes, public: true
   end
 
   def about
+    expires_in 5.minutes, public: true
 
   end
 
   def features
+    expires_in 5.minutes, public: true
 
   end
 
   def contribute
-
+    expires_in 5.minutes, public: true
   end
 end
--- config.ru
+++ config.ru
@@ -3,4 +3,5 @@
 require ::File.expand_path('../config/environment',  __FILE__)
 use Rack::Deflater
 use Rack::ETag
+use Rack::Cache
 run NimbusWebapp::Application
