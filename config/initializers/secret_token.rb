93c43df72a6f978da8a9a5e94cb38b16abd2a93c
Nick Gerleman <nick@nickgerleman.com>
2013-04-29 15:02:08 -0500

    Fixed Malformed Comment

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-NimbusWebapp::Application.config.secret_token ='796e487cadafaf3980e8bfa700abcf9068b461572a7495ed0b3fb3a10f6c8424e0374b859c60075764255af65c918b4c803e2de8ee90992a4e48d400903ab85d'
+NimbusWebapp::Application.config.secret_token = ENV['SECRET_TOKEN']
