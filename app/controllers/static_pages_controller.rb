27d5aca60209a3850ece5808ea9b04e91c73e4a0
Nick Gerleman <nick@nickgerleman.com>
2013-04-29 03:25:56 -0500

    Added Users model and controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,20 +1,17 @@
 class StaticPagesController < ApplicationController
+  caches_action(:home, :about, :features, :contribute)
   def home
-    expires_in 10.minutes, public: true
   end
 
   def about
-    expires_in 10.minutes, public: true
 
   end
 
   def features
-    expires_in 10.minutes, public: true
 
   end
 
   def contribute
-    expires_in 10.minutes, public: true
   end
 
 end
