f17d12fb104032a5be282731c572ca578c78854c
Nick Gerleman <nick@nickgerleman.com>
2013-04-29 04:20:48 -0500

    Added some validation and set git to ignore secret token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
+  before_save { |user| user.email = email.downcase }
+  #Taken from Rails Tutorial
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   attr_accessible :email, :name, :password_hash
+  validates :email, presence: true, length: {maximum: 50}, format: {with: VALID_EMAIL_REGEX},
+            uniqueness: {case_sensitive: false}
+  validates :name, presence: true, length: {maximum: 50, minimum: 2}
+  validates :password_hash, presence: true
 end
