6a6086d767b17aa99bf052fad16ebae84fdfa2a8
Nick Gerleman <nick@nickgerleman.com>
2013-04-29 10:00:58 -0500

    Added basic registration page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ NimbusWebapp::Application.routes.draw do
   match '/features', to: 'static_pages#features'
   match '/about', to: 'static_pages#about'
   match '/contribute', to: 'static_pages#contribute'
-
+  match '/register', to: 'users#new'
+  resource :users
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
