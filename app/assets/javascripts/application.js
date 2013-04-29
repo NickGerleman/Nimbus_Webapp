51b155a9875fa9010cd3057aeeee2adc0ad8633b
Nick Gerleman <nick@nickgerleman.com>
2013-04-29 01:20:26 -0500

    IE cookie stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,12 @@
 //= require jquery.cookie
 //= require bootstrap
 //= require_tree .
+function ignore_warning() {
+    $.cookie("ignore_warning", true, { expires: 30 })
+}
+
+function ie_warning() {
+    if ($.cookie('ignore_warning') == true) {
+        $('#ie_warning').modal()
+    }
+}
