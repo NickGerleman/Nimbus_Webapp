5df0d23ae53cd0d474cc664331dac7744790542e
Nick Gerleman <nick@nickgerleman.com>
2013-05-03 11:33:19 -0500

    js work

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,9 +1,21 @@
-function loadingBar() {
-    var register= $('#register');
-    register.on('shown', register.modal('hide'));
-    $('.modal-header').html('<h3>Please Wait...</h3>');
-    $('.modal-body').html('<div class="progress progress-striped active"></div>')
-    $('.progress').append('<div class="bar" style="width: 100%;"></div>');
-    $('.modal-footer').remove();
-    register.on('hidden', register.modal('show'));
-}
+function displayLoadingBar()
+    {
+        if (document.getElementById('loadingBar'))
+            $('#loadingBar').modal('show');
+        else
+            {
+                var loadingBarModal = $('<div class="modal hide fade" id="loadingBar" aria-hidden="true"></div>');
+                var header = $('<div class="modal-header">');
+                var heading = $('<h3>Please Wait...</h3>');
+                var body = $('<div class="modal-body"></div>')
+                var outerBar = $('<div class="progress progress-striped active"></div>');
+                var innerBar = $('<div class="bar" style="width: 100%;"></div>');
+                header.append(heading);
+                loadingBarModal.append(header);
+                outerBar.append(innerBar);
+                body.append(outerBar);
+                loadingBarModal.append(body);
+                $('body').append(loadingBarModal);
+                loadingBarModal.modal('show');
+            }
+    }
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new params[:user]
     respond_to do |format|
-    if verify_recaptcha(model: @user) && @user.save
+      if verify_recaptcha(model: @user) && @user.save
       format.js
     else
       format.js
