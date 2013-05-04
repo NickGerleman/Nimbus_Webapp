9dd87adf9e72b8caba881003d2d43863701bd0c2
Nick Gerleman <nick@nickgerleman.com>
2013-05-04 02:31:44 -0500

    Use Coffeescript for static js assets

--- app/assets/javascripts/custom.js
+++
@@ -1,9 +0,0 @@
-function ignore_warning() {
-    $.cookie("ignore_warning", true, { expires: 30 })
-}
-
-function ie_warning() {
-    if ($.cookie("ignore_warning") != "true") {
-        $('#ie_warning').modal()
-    }
-}
---
+++ app/assets/javascripts/custom.js.coffee
@@ -0,0 +1,5 @@
+ this.ignore_warning = ->
+  $.cookie("ignore_warning", true, { expires: 30 })
+
+this.ie_warning = ->
+  $('#ie_warning').modal() if $.cookie("ignore_warning") != "true"
