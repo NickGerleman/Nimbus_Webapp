cddb220e20088b51c9b5f17b70d2d2e4a5143910
Nick Gerleman <nick@nickgerleman.com>
2013-05-14 18:25:27 -0500

    fixing email again

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ActionMailer::Base
   default from: 'nimbusdev@nickgerleman.com'
 
   def verify_email(user)
-    @token = verify_url
+    @token = user.email_token
     mail to: user.email, subject: 'Verify Your account'
   end
 end
