11e2a55b24556cc52f70358b59b7b3ebe7dc5d37
Nick Gerleman <nick@nickgerleman.com>
2013-05-14 09:35:40 -0500

    cache home action

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
-  caches_action(:new_user_home, :about, :features, :contribute)
+  caches_action(:home, :about, :features, :contribute)
 
   def home
 
