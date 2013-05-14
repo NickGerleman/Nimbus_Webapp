42edfb4a1f5b67c33e0a1a54c21cd82fa76fd55b
Nick Gerleman <nick@nickgerleman.com>
2013-05-14 18:11:36 -0500

    less hackish link in mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ActionMailer::Base
   default from: 'nimbusdev@nickgerleman.com'
 
   def verify_email(user)
-    @token = user.email_token
+    @token = verify_url
     mail to: user.email, subject: 'Verify Your account'
   end
 end
