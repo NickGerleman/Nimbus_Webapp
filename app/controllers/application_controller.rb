a17a0d96a68fd936f0f894b27c942d3796f202e6
Nick Gerleman <nick@nickgerleman.com>
2013-06-15 07:59:44 -0500

    CSS and foundation fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,20 +4,16 @@ class ApplicationController < ActionController::Base
   def default_url_options(opts={})
     ENV['HOST'] = ENV['HOST'] || '127.0.0.1:3000'
     Rails.env.production? ? protocol = 'https' : protocol = 'http'
-    opts.merge(
-        {
-            protocol: protocol,
-            host: ENV['HOST']
-        })
+    opts.merge({ protocol: protocol, host: ENV['HOST']})
   end
 
   # The object representing the current user or nil if no user is logged in
   def current_user
-    @user ||= -> do
+    @user ||= (
       #session can be stored in session cookie or independently depending on whether remember me was checked
       token = session[:session_token] || cookies[:session_token]
       token.blank? ? nil : Session.get_user(token)
-    end.call
+    )
   end
 
   helper_method :current_user
