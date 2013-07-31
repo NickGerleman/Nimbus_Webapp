15c6f823c8837f749e7b4784a8cfe68236b9330e
Nick Gerleman <nick@nickgerleman.com>
2013-07-31 16:13:51 -0500

    fix mailer issue for production

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,15 +1,15 @@
 class UserMailer < ActionMailer::Base
-  default from: 'nimbusdev@nickgerleman.com'
+  default from: 'Nimbus Support <support@nimbuu.us>'
 
   def verify_email(user_id)
     user = User.find user_id
     @token = user.email_token
-    mail to: user.email, subject: 'Verify Your Nimbus Account', from: 'Nimbus Support'
+    mail to: user.email, subject: 'Verify Your Nimbus Account'
   end
 
   def reset_password(user_id)
     user = User.find user_id
     @token = user.password_reset_token
-    mail to: user.email, subject: 'Reset Your Nimbus Password', from: 'Nimbus Support'
+    mail to: user.email, subject: 'Reset Your Nimbus Password'
   end
 end
