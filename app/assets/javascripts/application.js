1647b9b477d5dabb9e0b43444de18872c1d8a6e0
Nick Gerleman <nick@nickgerleman.com>
2013-08-17 03:15:31 -0500

    only load specific parts of foundation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,11 @@
 //= require jquery_ujs
 //= require jquery.spin
 //= require jquery-fileupload/basic
-//= require foundation
+//= require foundation/foundation
+//= require foundation/foundation.alerts
+//= require foundation/foundation.dropdown
+//= require foundation/foundation.forms
+//= require foundation/foundation.topbar
 //= require modernizr
 //= require magnific-popup
 //= require_tree .
