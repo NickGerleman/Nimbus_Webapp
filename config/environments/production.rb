e2299c54a3031b68b7affc1738a2bbc613f22a48
Nick Gerleman <nick@nickgerleman.com>
2013-12-11 08:51:23 -0600

    fix configuration issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,8 +17,8 @@ NimbusWebapp::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
   # Compress JavaScripts and CSS
-  config.js_compressor = :uglifier
-  config.assets.compress = true
+  config.assets.js_compressor = :uglifier
+  config.assets.css_compressor = :yui
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
@@ -30,7 +30,7 @@ NimbusWebapp::Application.configure do
 
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
